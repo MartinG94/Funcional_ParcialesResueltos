eb672a0da4f889b8d0ae2bc611811b606f99a121
Unknown <guillenmartin94@gmail.com>
2018-05-22 15:49:50 -0300

    Comentarios
    _ Se agregaron comentarios para aclarar de que punto es cada función.

--- obrasSociales.hs
+++ obrasSociales.hs
@@ -6,6 +6,7 @@ import Test.Hspec
 
 -- Parte A
 
+-- Punto 1)
 type Enfermedad = String
 data Sexo = Femenino | Masculino deriving (Show, Eq)
 type Peso = Float
@@ -18,9 +19,6 @@ data Socio = Socio {
   preexistencia :: [Enfermedad]
 } deriving (Show, Eq)
 
-josé = Socio Masculino 22 78.92 ["zamorreaDistópica"]
-analía = Socio Femenino 34 70 []
-
 type Dinero = Float
 type Cantidad = Int
 
@@ -30,12 +28,16 @@ data Tratamiento = Tratamiento {
   enfermedad :: Enfermedad
 } deriving (Show, Eq)
 
+josé = Socio Masculino 22 78.92 ["zamorreaDistópica"]
+analía = Socio Femenino 34 70 []
+
 x1 = Tratamiento 5000 30 "zamorreaDistópica"
 xfg23 = Tratamiento 10000 2 "zamorreaDistópica"
 
 nuevaEnfermedad unaEnfermedad socio = socio {preexistencia = unaEnfermedad}
 agregarPreexistencia otraEnfermedad socio = nuevaEnfermedad (otraEnfermedad : preexistencia socio) socio
 
+-- Punto 2)
 diagnosticarPreexistencia :: Enfermedad -> Socio -> Socio
 diagnosticarPreexistencia = agregarPreexistencia
 
@@ -43,11 +45,13 @@ esObeso = (> 150) . peso
 tieneEdadAvanzada = (> 75) . edad
 tieneMuchasPreexistencias = (> 8) . length . preexistencia
 
+-- Punto 3)
 estáEnRiesgo :: Socio -> Bool
 estáEnRiesgo socio = esObeso socio || tieneEdadAvanzada socio || tieneMuchasPreexistencias socio
 
 -- Parte B
 
+-- Punto 4)
 data Solicitud = Solicitud {
   socio:: Socio,
   tratamiento::Tratamiento
@@ -61,6 +65,7 @@ type Prestación = Solicitud -> Cobertura
 costoDeSolicitud = costoBase . tratamiento
 enfermedadDeLaSolicitud = enfermedad . tratamiento
 
+-- Punto 5a)
 prestaciónTotal :: Enfermedad -> Prestación
 prestaciónTotal unaEnfermedad solicitud
   | ((== unaEnfermedad) . enfermedadDeLaSolicitud) solicitud = costoDeSolicitud solicitud
@@ -69,19 +74,24 @@ prestaciónTotal unaEnfermedad solicitud
 laEnfermedadEsPreexistente unaEnfermedad = elem unaEnfermedad . preexistencia
 laEnfermedadNoEsPreexistente unaEnfermedad = not . laEnfermedadEsPreexistente unaEnfermedad
 
+-- Punto 5b)
 prestaciónSinPreexistencias :: Enfermedad -> Prestación
 prestaciónSinPreexistencias unaEnfermedad solicitud
   | laEnfermedadNoEsPreexistente (enfermedadDeLaSolicitud solicitud) (socio solicitud) = ((*0.5) . costoDeSolicitud) solicitud897
   | otherwise = 0
 
+-- Punto 5c)
 prestaciónHastaMáximo :: Dinero -> Prestación
 prestaciónHastaMáximo cantidadQueCubre = min cantidadQueCubre . costoDeSolicitud
 
+-- Punto 5d)
 nada :: Prestación
 nada _ = 0
 
+-- Punto 6a)
 sumarPrestaciones :: Prestación -> Prestación -> Prestación
 sumarPrestaciones unaPrestación otraPrestación solicitud = max (unaPrestación solicitud) (otraPrestación solicitud)
 
+-- Punto 6b)
 plan :: [Prestación] -> Prestación
 plan = foldr sumarPrestaciones nada
